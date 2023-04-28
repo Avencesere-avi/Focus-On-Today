55ae19866fc11cf87bc68f3c382ccd65bcfe49b5
Ravi Kumar <raviavi2002@gmail.com>
2023-04-29 01:55:01 +0530

    "added div in p(error-message)"

--- script.js
+++ script.js
@@ -3,16 +3,39 @@ const checkBoxList = document.querySelectorAll(".goal-select");
 const containertask = document.querySelectorAll(".container-task");
 
 
-const goalinput = document.querySelector(".goal-input")
+const goalinput = document.querySelectorAll(".goal-input")
 
-checkBoxList.forEach((checkbox)=>{
+const error = document.querySelector(".progress-error");
 
-    checkbox.addEventListener("click",(e)=>{
 
-        checkbox.parentElement.classList.toggle("completed");
-       
-       });
+
+
+
+
+
+checkBoxList.forEach((eachcheckbox)=>{
+
+      eachcheckbox.addEventListener("click",(e)=>{
+ 
+            const inputFields = [...goalinput].every((x)=>{return x.value});
+           
+            if(inputFields){
+              eachcheckbox.parentElement.classList.toggle("completed")
+            }
+
+            else{
+
+                  error.parentElement.classList.add("ok")
+
+            }
+
+            
+
+      });
 });
+
+
+goalinput.forEach((e)=>{e.addEventListener("focus",(e)=>{error.parentElement.classList.remove("ok")})})
 
 
 
